732ce5c04b6a0edb34a14ca920c5f7a83c10a806
Daniel Nobre <55868687+Dan-Nobre@users.noreply.github.com>
2025-07-21 17:10:37 -0300

    Initial Commit

---
+++ Navigation Exampel/Navigation_ExampelApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_ExampelApp.swift
+//  Navigation Exampel
+//
+//  Created by Daniel Araujo Nobre on 21/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_ExampelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
